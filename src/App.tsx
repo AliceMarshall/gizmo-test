8fff762ebfce71fd6d045cc0f86cd12192b4cdf3
Alice Marshall <marshall.alice92@gmail.com>
2023-11-09 08:06:41 +0000

    Adding styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+    padding: 4em;
+`;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #bf4f74;
+`;
 
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+        <Wrapper>
+            <Title>Hello World</Title>
+        </Wrapper>
+    );
 };
 
 export default App;
